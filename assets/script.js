4e4d8e4f2549c97454f6ad10fe43564d517c5bb1
New Name <mubin.momin@nxp.com>
2020-05-26 15:02:49 -0500

    psuedo coded JS file

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+// create variable to store input value from user
+
+// store API key as global variable
+
+// get value from users input
+
+// clear input box initially
+
+// Query URL call variable.
+
+// use ajax to get the information needed from the api to display on the screen. 
+
+// function for current conditions
+
+// add attributes to page that need to appended onto the screen. 
