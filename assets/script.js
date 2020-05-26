d0f57a871e4d29b373990c544af00fe8a8ba8898
New Name <mubin.momin@nxp.com>
2020-05-26 15:47:35 -0500

    current weather function

--- assets/script.js
+++ assets/script.js
@@ -59,4 +59,30 @@ $.ajax({
 
 // function for current conditions
 
+function getCurrentConditions (response) {
+
+  // get the temperature and convert to fahrenheit 
+  let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+  tempF = Math.floor(tempF);
+
+  $('#currentCity').empty();
+
+  // get and set the content 
+  const card = $("<div>").addClass("card");
+  const cardBody = $("<div>").addClass("card-body");
+  const city = $("<h4>").addClass("card-title").text(response.name);
+  const cityDate = $("<h4>").addClass("card-title").text(date.toLocaleDateString('en-US'));
+  const temperature = $("<p>").addClass("card-text current-temp").text("Temperature: " + tempF + " °F");
+  const humidity = $("<p>").addClass("card-text current-humidity").text("Humidity: " + response.main.humidity + "%");
+  const wind = $("<p>").addClass("card-text current-wind").text("Wind Speed: " + response.wind.speed + " MPH");
+  const image = $("<img>").attr("src", "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
+
+  // add to page
+  city.append(cityDate, image)
+  cardBody.append(city, temperature, humidity, wind);
+  card.append(cardBody);
+  $("#currentCity").append(card)
+ 
+}
+
 // add attributes to page that need to appended onto the screen. 
